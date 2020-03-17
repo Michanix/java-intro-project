526b93bc23a531cf284978b5039aae007b07c100
mihhail.m <mihhail.matisinets@gmail.com>
2020-03-17 11:05:01 +0200

    now can create character, abilities vals assinged automaticly

--- main/components/Character.java
+++ main/components/Character.java
@@ -1,5 +1,49 @@
 package components;
 
+import utils.Abilities;
+
 public class Character {
+    private String name;
     private String race;
+    private Abilities abilities;
+
+    public Character(String name, String race) {
+        this.name = name;
+        this.race = race;
+        this.abilities = new Abilities();
+    }
+
+    // get and set
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public Abilities getAbilities() {
+        return abilities;
+    }
+
+    public void setAbilities(Abilities abilities) {
+        this.abilities = abilities;
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "name='" + name + '\'' +
+                ", race='" + race + '\'' +
+                ", abilities=" + abilities +
+                '}';
+    }
 }
