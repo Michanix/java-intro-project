4feaf0e92903e8e0c640aa804d2a2bf28cac4f1a
mihhail.m <mihhail.matisinets@gmail.com>
2020-03-17 19:59:48 +0200

    cleanup

--- main/main/Main.java
+++ main/main/Main.java
@@ -1,27 +1,13 @@
 package main;
 
 import components.*;
-import utils.Abilities;
 import utils.DiceType;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<DiceType> dieTypeList = new ArrayList<>(List.of(DiceType.values()));
         UI ui = new UI();
-        CreateSummonerUI createSummonerUI = new CreateSummonerUI();
-        createSummonerUI.run();
-
-        /*
-        for (DiceType die: dieTypeList) {
-            System.out.println(die);
-        }
-        System.out.println("Choose die to roll: ");
-        ReadInput input = new ReadInput();
-        Dice die1 = new Dice(input.getUserDice());
-        System.out.printf("You rolled %s\n", die1.getName());
-        System.out.printf("The number is %d", die1.rollTheDie());
-         */
+        ui.run();
     }
 }
