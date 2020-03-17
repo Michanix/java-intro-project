6116f86c9eee7abaec0b33a5c91a40ec66567b87
mihhail.m <mihhail.matisinets@gmail.com>
2020-03-17 12:32:49 +0200

    small tweaks

--- main/utils/GameRace.java
+++ main/utils/GameRace.java
@@ -1,4 +1,11 @@
 package utils;
 
 public enum GameRace {
+    ELF() { public String getRaceName() { return "Elf"; }},
+    MOUNTAINDWARF() { public String getRaceName() { return "Mountain Dwarf"; }},
+    HUMAN() { public String getRaceName() { return "Human"; }},
+    HIGHELF() { public String getRaceName() { return "High Elf"; }};
+
+    public abstract String getRaceName();
 }
+
