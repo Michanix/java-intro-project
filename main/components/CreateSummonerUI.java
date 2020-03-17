4c164ff373f8f080d9d7e33019051417a6464bc0
mihhail.m <mihhail.matisinets@gmail.com>
2020-03-17 12:31:54 +0200

    added while loop for recovery

--- main/components/CreateSummonerUI.java
+++ main/components/CreateSummonerUI.java
@@ -1,5 +1,7 @@
 package components;
 
+import utils.GameRace;
+
 public class CreateSummonerUI {
     private String name;
     private ReadInput userInput = new ReadInput();
@@ -8,22 +10,42 @@ public class CreateSummonerUI {
         System.out.println("How shall we call you, summoner?");
         System.out.print("> ");
         String name = userInput.getUserInput();
-        System.out.println("What is your race? ");
-        raceOptions();
-        System.out.print("> ");
-        String race = userInput.getUserInput();
+        GameRace race = null;
+        while (race == null) {
+            System.out.println("What is your race? ");
+            raceOptions();
+            String option = userInput.getUserInput();
+            switch (option) {
+                default:
+                    System.err.println("Invalid option. Please select option from the list!");
+                    break;
+                case "1":
+                    race = GameRace.ELF;
+                    break;
+                case "2":
+                    race = GameRace.MOUNTAINDWARF;
+                    break;
+                case "3":
+                    race = GameRace.HUMAN;
+                    break;
+                case "4":
+                    race = GameRace.HIGHELF;
+                    break;
+            }
+        }
         Summoner newSummoner = new Summoner(name, race);
         System.out.println("Welcome to the Great Adventure, " + name + "!");
-        System.out.println("Your game race: " + race);
+        System.out.println("Your game race: " + race.getRaceName());
         System.out.println("You initial abilities: ");
         System.out.println(newSummoner.getAbilities());
     }
 
     private void raceOptions() {
         System.out.println("[1] Elf");
-        System.out.println("[2] Dwarf");
+        System.out.println("[2] Mountain Dwarf");
         System.out.println("[3] Human");
-        System.out.println("[4] Dark elf");
+        System.out.println("[4] High elf");
+        System.out.print("> ");
     }
 
     public void run() {
