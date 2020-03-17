cc927a6ae4e7512198647b206a8c15326f3e469a
mihhail.m <mihhail.matisinets@gmail.com>
2020-03-17 11:30:00 +0200

    no functionality, only display

--- main/components/UI.java
+++ main/components/UI.java
@@ -6,14 +6,6 @@ public class UI {
         System.out.println("=== Welcome To the Dungeons and Dragons ===");
     }
 
-    private void askName() {
-        System.out.println("How shall We call you summoner? ");
-        System.out.print("> ");
-        ReadInput readInput = new ReadInput();
-        String name = readInput.getUserInput();
-        System.out.println("Welcome, " + name + "!");
-    }
-
     private void menu() {
         System.out.println("===================");
         System.out.println("===Choose action===");
@@ -21,15 +13,14 @@ public class UI {
     }
 
     private void options() {
-        System.out.println("[1] Start the game");
-        System.out.println("[2] Create character");
+        System.out.println("[1] Create character");
+        System.out.println("[2] Start the game");
         System.out.println("[3] Exit");
         System.out.print("> ");
     }
 
     public void run() {
         greeting();
-        askName();
         menu();
         options();
     }
