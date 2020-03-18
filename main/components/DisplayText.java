168684937218be7d1d278e87131fa0559a3e80e4
mihhail.m <mihhail.matisinets@gmail.com>
2020-03-18 09:36:05 +0200

    special class to display text in game, and class to read text from files

---
+++ main/components/DisplayText.java
@@ -0,0 +1,18 @@
+package components;
+
+import java.util.List;
+
+public class DisplayText {
+    private List<String> listOfText;
+
+    public DisplayText(String path) throws Exception {
+        ReadTextFile readTextFile = new ReadTextFile();
+        this.listOfText = readTextFile.readFileToList(path);
+    }
+
+    public void printText() {
+        for (String sentence: listOfText) {
+            System.out.println(sentence);
+        }
+    }
+}
---
+++ main/components/ReadTextFile.java
@@ -0,0 +1,30 @@
+package components;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadTextFile {
+    public List<String> readFileToList(String path) throws Exception {
+        // Takes in text file with some data and returns it as ArrayList<>()
+        // Assumed that data on seperate line
+        BufferedReader in = null;
+        List<String> listOFText = new ArrayList<>();
+        try {
+            in = new BufferedReader(new FileReader(path));
+            String str;
+            while ((str = in.readLine()) != null) {
+                listOFText.add(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+        return listOFText;
+    }
+}
